25413a7812a07af982b7f3c1119f1918c5597de7
sitota49 <se.sitota.ezra@gmail.com>
2021-11-17 20:38:32 +0300

    Laravel Blog Intro

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::orderBy('created_at')->get();
+
+        return view('posts.index')->with([
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required|string',
+            'body' => 'required|string'
+        ]);
+       
+
+        // Create a new post
+        $post = new Post;
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+
+        return redirect()->route('posts.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <h1>Posts</h1>
+    <a href="{{ route('posts.create') }}" class="btn btn-primary">Add New</a>
+    @if($posts->count() > 0)
+        @foreach($posts as $post)
+            <div class="card m-1">
+                <div class="card-header">
+                  <p class="card-title">{{ $post->title }}</p>  
+                </div>
+                <div class="card-body">
+                    {{ $post->body }}
+
+                    <hr>
+                    <a href="{{ route('posts.edit', $post->id) }}"></a>
+                </div>
+
+            </div>
+        @endforeach
+    @else
+        <div class="card card-body m-1">
+            <h4>No Post Found</h4>
+        </div>
+    @endif
+</div>
+
+@endsection

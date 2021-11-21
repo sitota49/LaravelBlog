6725c901375f41c7868bcbcf6e7f2abc3c63d45c
sitota49 <se.sitota.ezra@gmail.com>
2021-11-21 09:59:32 +0300

    web views update

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,25 +6,24 @@
    
     <h1>Posts</h1>
    
-    <a href="{{ route('posts.create') }}" class="btn btn-primary float-right ">Add New</a>
+  
 <div class=" my-5 py-3">
-     <div class="card mb-4">
-        <h5 class="card-header">Search</h5>
-        <div class="card-body">
-            <form action="{{ route('posts.index') }}" method="GET" role="search">
+    <h5>Search for Posts</h5>
+     <form action="{{ route('posts.index') }}" method="GET" role="search">
                 <div class="input-group">
+                         
                      <select class="form-control" name="q">
+                         <option value="" >Pick a category</option>
                         @foreach($categories as $cateogry)
                         <option value="{{$cateogry->id}}">{{$cateogry->name}}</option>
                         @endforeach
                     </select>
                     <div class="input-group-append">
-                    <button class="btn btn-primary" type="submit"">Search</button>
+                    <button class="btn btn-secondary" type="submit"">Search</button>
                     </div>
                 </div>
             </form>
-        </div>
-    </div>
+    
     </div>
     
     @if($posts->count() > 0)
@@ -37,7 +36,7 @@
                     </div>
                     <div class="col-8 ">
                         <div class="container align-middle">
-                        <h3 clas><a href="{{route('posts.show', $post->id) }}">{{$post->title}}</a></h3>
+                        <h3 clas><a href="{{route('posts.show', $post->id) }}" style="text-decoration:none">{{$post->title}}</a></h3>
                           <small>
                        @for ($i = 0; $i < $categories->count(); $i++)
                     @if ($categories[$i]->id == $post->cat_id)

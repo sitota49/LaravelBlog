e265eda962c509bd16534c2275e8150a93b989a5
sitota49 <se.sitota.ezra@gmail.com>
2021-11-21 09:21:51 +0300

    API Controller Added

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Category;
+
+class ApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+         return Post::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Post::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+     public function search($title)
+    {
+        return Post::where('title', 'like', '%'.$title.'%')->get();
+    }
+
+     public function searchByCategory($cat_id)
+    {
+        return Post::where('cat_id','like','%'.$cat_id.'%')->orderBy('created_at')->get();
+    }
+
+     public function allCategories()
+    {
+        return Category::all();
+    }
+
+    public function findCategoryById($id)
+    {
+        return Category::find($id);
+    }
+
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ApiController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/posts', [ApiController::class, 'index']);
+Route::get('/posts/{id}', [ApiController::class, 'show']);
+Route::get('/posts/search/{title}', [ApiController::class, 'search']);
+Route::get('/posts/searchByCategory/{cat_id}', [ApiController::class, 'searchByCategory']);
+Route::get('/categories', [ApiController::class, 'allCategories']);
+Route::get('/categories/{id}', [ApiController::class, 'findCategoryById']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
